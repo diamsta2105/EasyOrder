f0e06bb11fbee92938223945684e8da61d9ef932
diamsta2105 <steveports92@gmail.com>
2026-07-12 12:51:02 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,18 @@
 const productsDatabase = [
 
     {
-        code: "32501016",
-        description: "Σφιγκτήρας 10-16",
+        code: "3250812",
+        description: "Σφιγκτήρας 8-12",
         category: "Σφιγκτήρες",
-        price: 0,
+        price: 0.42,
         discount: 0
     },
 
     {
-        code: "3250812",
-        description: "Σφιγκτήρας 8-12",
+        code: "32501016",
+        description: "Σφιγκτήρας 10-16",
         category: "Σφιγκτήρες",
-        price: 0,
+        price: 0.00,
         discount: 0
     }
 

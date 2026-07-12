f0fec3c69441d96a5aa7d812e9d03819751cee47
diamsta2105 <steveports92@gmail.com>
2026-07-12 11:45:08 +0300

    Fix product codes format

--- data.js
+++ data.js
@@ -1,50 +1,22 @@
-// Βάση προϊόντων Easy Order
-
 const productsDatabase = [
+
+    {
+        code: "32501016",
+        description: "Σφιγκτήρας 10-16",
+        category: "Σφιγκτήρες",
+        price: 0,
+        discount: 0
+    },
 
     {
         code: "3250812",
         description: "Σφιγκτήρας 8-12",
-        price: 0.42,
-        discount: 0
-    },
-
-    {
-        code: "3251020",
-        description: "Σφιγκτήρας 10-20",
-        price: 0.48,
-        discount: 0
-    },
-
-    {
-        code: "13026520",
-        description: "Στριφώνι 6,5x20",
-        price: 0.15,
-        discount: 0
-    },
-
-    {
-        code: "4500816",
-        description: "Βίδα Μ8x16",
-        price: 0.12,
+        category: "Σφιγκτήρες",
+        price: 0,
         discount: 0
     }
 
 ];
 
 
-
-// Βάση πελατών (δοκιμαστική)
-
-const customersDatabase = [
-
-    {
-        name: "ΔΟΚΙΜΑΣΤΙΚΟΣ ΠΕΛΑΤΗΣ",
-        area: "Αθήνα",
-        profession: "Συνεργείο",
-        afm: "000000000",
-        doy: "",
-        phone: ""
-    }
-
-];
+const customersDatabase = [];

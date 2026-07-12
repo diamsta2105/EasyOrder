ed04b769c5b8d5cbe20d8c0b1c5767a49667789b
diamsta2105 <steveports92@gmail.com>
2026-07-12 09:49:53 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,50 @@
+// Βάση προϊόντων Easy Order
+
+const productsDatabase = [
+
+    {
+        code: "3250812",
+        description: "Σφιγκτήρας 8-12",
+        price: 0.42,
+        discount: 0
+    },
+
+    {
+        code: "3251020",
+        description: "Σφιγκτήρας 10-20",
+        price: 0.48,
+        discount: 0
+    },
+
+    {
+        code: "13026520",
+        description: "Στριφώνι 6,5x20",
+        price: 0.15,
+        discount: 0
+    },
+
+    {
+        code: "4500816",
+        description: "Βίδα Μ8x16",
+        price: 0.12,
+        discount: 0
+    }
+
+];
+
+
+
+// Βάση πελατών (δοκιμαστική)
+
+const customersDatabase = [
+
+    {
+        name: "ΔΟΚΙΜΑΣΤΙΚΟΣ ΠΕΛΑΤΗΣ",
+        area: "Αθήνα",
+        profession: "Συνεργείο",
+        afm: "000000000",
+        doy: "",
+        phone: ""
+    }
+
+];

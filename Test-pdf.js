dd8bbb1324fef14db43ab8e3bc182fbd5410b391
diamsta2105 <steveports92@gmail.com>
2026-07-15 19:58:49 +0300

    Update Test-pdf.js

--- Test-pdf.js
+++ Test-pdf.js
@@ -123,7 +123,7 @@ function downloadPDF(order) {
             },
             columnStyles: {
                 0: { cellWidth: 25 },
-                1: { cellWidth: 'auto' },
+                
                 2: { cellWidth: 15, halign: 'right' },
                 3: { cellWidth: 25, halign: 'right' },
                 4: { cellWidth: 20, halign: 'center' },

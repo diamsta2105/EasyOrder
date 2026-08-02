4b2c54bf7e3a24c6cffbff7053c47361ac3651f1
diamsta2105 <steveports92@gmail.com>
2026-08-02 16:41:07 +0300

    Update Test-pdf.js

--- Test-pdf.js
+++ Test-pdf.js
@@ -242,7 +242,8 @@ function downloadPDF(order) {
         let fileName = "Παραγγελία-" + (order.customer || "FÖRCH") + ".pdf";
         doc.save(fileName);
 
-    } catch (error) {
+    } 
+    catch (error) {
         alert("Κάτι πήγε στραβά κατά τη δημιουργία του PDF: " + error.message);
     }
 }
